61dec9769eac6864012cd364388bdc78db623683
abdelmounim <abdelmounimbaouia@gmail.com>
2024-08-25 20:41:43 +0100

    test: arrays + invalid test cases

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -3,7 +3,9 @@ package parser
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -22,19 +24,19 @@ func TestRESPParser(t *testing.T) {
 		{"EmptyBulkString", "$0\r\n\r\n", &Token{tType: BulkString, value: []byte("")}, false},
 		{"Boolean True", "#t\r\n", &Token{Boolean, true}, false},
 		{"Boolean False", "#f\r\n", &Token{Boolean, false}, false},
-		// {"Array", "*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n", []interface{}{"hello", "world"}, false},
-		// {"NestedArray", "*2\r\n*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n$6\r\nfoobar\r\n", []interface{}{[]interface{}{"hello", "world"}, "foobar"}, false},
-		// {"PingCommand", "*1\r\n$4\r\nping\r\n", []interface{}{"ping"}, false},
-		// {"EchoCommand", "*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n", []interface{}{"echo", "hello world"}, false},
-		// {"GetCommand", "*2\r\n$3\r\nget\r\n$3\r\nkey\r\n", []interface{}{"get", "key"}, false},
+		{"Array", "*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n", &Token{Array, []*Token{{BulkString, []byte("hello")}, {BulkString, []byte("world")}}}, false},
+		{"NestedArray", "*2\r\n*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n$6\r\nfoobar\r\n", &Token{Array, []*Token{{Array, []*Token{{BulkString, []byte("hello")}, {BulkString, []byte("world")}}}, {BulkString, []byte("foobar")}}}, false},
+		{"PingCommand", "*1\r\n$4\r\nping\r\n", &Token{Array, []*Token{{BulkString, []byte("ping")}}}, false},
+		{"EchoCommand", "*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n", &Token{Array, []*Token{{BulkString, []byte("echo")}, {BulkString, []byte("hello world")}}}, false},
+		{"GetCommand", "*2\r\n$3\r\nget\r\n$3\r\nkey\r\n", &Token{Array, []*Token{{BulkString, []byte("get")}, {BulkString, []byte("key")}}}, false},
 		//
 		// // Invalid cases
-		// {"InvalidBulkStringLength", "$5\r\nhell\r\n", nil, true},
-		// {"InvalidArrayLength", "*2\r\n$5\r\nhello\r\n", nil, true},
-		// {"MissingCRLF", "$5\r\nhello", nil, true},
-		// {"InvalidDataType", "x5\r\nhello\r\n", nil, true},
-		// {"EmptyInput", "", nil, true},
-		// {"IncompleteArray", "*2\r\n$5\r\nhello\r\n$5\r\nworl", nil, true},
+		{"InvalidBulkStringLength", "$5\r\nhell\r\n", nil, true},
+		{"InvalidArrayLength", "*2\r\n$5\r\nhello\r\n", nil, true},
+		{"MissingCRLF", "$5\r\nhello", nil, true},
+		{"InvalidDataType", "x5\r\nhello\r\n", nil, true},
+		{"EmptyInput", "", nil, true},
+		{"IncompleteArray", "*2\r\n$5\r\nhello\r\n$5\r\nworl", nil, true},
 	}
 
 	b := bytes.NewBuffer(nil) // Create an empty buffer
@@ -66,9 +68,75 @@ func TestRESPParser(t *testing.T) {
 				return
 			}
 
-			if !reflect.DeepEqual(result, tt.expected) {
+			if !deepEqualTokens(result, tt.expected) {
 				t.Errorf("Parse() = %v, want %v", result, tt.expected)
 			}
 		})
+	}
+}
+
+// deepEqualTokens compares two Tokens for deep equality
+func deepEqualTokens(a, b *Token) bool {
+	if a == nil || b == nil {
+		return a == b
+	}
+	if a.tType != b.tType {
+		return false
+	}
+	switch a.tType {
+	case Array:
+		aArr, aOk := a.value.([]*Token)
+		bArr, bOk := b.value.([]*Token)
+		if !aOk || !bOk {
+			fmt.Printf("Array type assertion failed: aOk = %v, bOk = %v\n", aOk, bOk)
+			fmt.Printf("a.value type: %T, b.value type: %T\n", a.value, b.value)
+			return false
+		}
+		if len(aArr) != len(bArr) {
+			fmt.Println(2)
+			return false
+		}
+		for i := range aArr {
+			if !deepEqualTokens(aArr[i], bArr[i]) {
+				fmt.Println(3)
+				return false
+			}
+		}
+		return true
+	// case BulkString:
+	// 	aStr, aOk := a.value.([]byte)
+	// 	bStr, bOk := b.value.([]byte)
+	// 	return aOk && bOk && bytes.Equal(aStr, bStr)
+	// Add cases for other types as needed
+	default:
+		return reflect.DeepEqual(a.value, b.value)
+	}
+}
+
+// tokenToString converts a Token to a string representation for error messages
+func tokenToString(t *Token) string {
+	if t == nil {
+		return "<nil>"
+	}
+	switch t.tType {
+	case Array:
+		arr, ok := t.value.([]*Token)
+		if !ok {
+			return "<invalid array>"
+		}
+		elements := make([]string, len(arr))
+		for i, elem := range arr {
+			elements[i] = tokenToString(elem)
+		}
+		return fmt.Sprintf("Array[%s]", strings.Join(elements, ", "))
+	case BulkString:
+		str, ok := t.value.([]byte)
+		if !ok {
+			return "<invalid bulk string>"
+		}
+		return fmt.Sprintf("BulkString[%s]", string(str))
+	// Add cases for other types as needed
+	default:
+		return fmt.Sprintf("%v", t.value)
 	}
 }
