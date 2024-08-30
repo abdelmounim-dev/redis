8b72f694524638bf94ae94989be34b50e4d34080
abdelmounim <abdelmounimbaouia@gmail.com>
2024-08-30 22:52:38 +0100

    feat: storage

---
+++ pkg/storage/storage.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/abdelmounim-dev/redis/pkg/parser"
+)
+
+type KeyValueStore struct {
+	data map[string]*parser.Token
+	mu   sync.RWMutex
+}
+
+func (s *KeyValueStore) Set(key string, value *parser.Token) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data[key] = value
+	return nil
+}
+
+func (s *KeyValueStore) Get(key string) (*parser.Token, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	t, ok := s.data[key]
+	if !ok {
+		return nil, fmt.Errorf("STORE GET: value with key %v not found", key)
+	}
+	return t, nil
+}
+
+func (s *KeyValueStore) Delete(key string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.data, key)
+
+	return nil
+}
