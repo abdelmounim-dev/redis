52ef702c486c7dbf655a7d6142a7c19f4cedab44
abdelmounim <abdelmounimbaouia@gmail.com>
2024-08-31 19:21:46 +0100

    feat: set and get commands

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,21 +2,37 @@ package handlers
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/abdelmounim-dev/redis/pkg/parser"
+	"github.com/abdelmounim-dev/redis/pkg/storage"
 )
 
-func HandleCommand(t *parser.Token) (*parser.Token, error) {
+type Handlers struct {
+	store storage.Store
+}
+
+func NewHandlers(store storage.Store) *Handlers {
+	return &Handlers{store: store}
+}
+
+func (h *Handlers) HandleCommand(t *parser.Token) (*parser.Token, error) {
 	if t.Type == parser.Array {
 		if len(t.Value.([]*parser.Token)) == 0 {
 			return &parser.Token{Type: parser.SimpleError, Value: "Empty Command"}, nil
 		}
 
-		switch string(t.Value.([]*parser.Token)[0].Value.([]byte)) {
-		case "PING":
-			return handlePing(t)
-		case "ECHO":
-			return handleEcho(t)
+		key := strings.ToLower(string(t.Value.([]*parser.Token)[0].Value.([]byte)))
+
+		switch key {
+		case "ping":
+			return h.handlePing(t)
+		case "echo":
+			return h.handleEcho(t)
+		case "set":
+			return h.handleSet(t)
+		case "get":
+			return h.handleGet(t)
 		default:
 			return &parser.Token{Type: parser.SimpleError, Value: "Command Not Supported"}, nil
 		}
@@ -24,7 +40,7 @@ func HandleCommand(t *parser.Token) (*parser.Token, error) {
 	return &parser.Token{Type: parser.SimpleError, Value: "Command Not Supported"}, nil
 }
 
-func handlePing(t *parser.Token) (*parser.Token, error) {
+func (h *Handlers) handlePing(t *parser.Token) (*parser.Token, error) {
 	if len(t.Value.([]*parser.Token)) == 1 {
 		return &parser.Token{Type: parser.SimpleString, Value: "PONG"}, nil
 	}
@@ -36,7 +52,7 @@ func handlePing(t *parser.Token) (*parser.Token, error) {
 	}
 	return t.Value.([]*parser.Token)[1], nil
 }
-func handleEcho(t *parser.Token) (*parser.Token, error) {
+func (h *Handlers) handleEcho(t *parser.Token) (*parser.Token, error) {
 	if len(t.Value.([]*parser.Token)) > 2 {
 		return &parser.Token{
 			Type:  parser.SimpleError,
@@ -44,4 +60,61 @@ func handleEcho(t *parser.Token) (*parser.Token, error) {
 		}, nil
 	}
 	return t.Value.([]*parser.Token)[1], nil
+}
+
+func (h *Handlers) handleSet(t *parser.Token) (*parser.Token, error) {
+
+	if len(t.Value.([]*parser.Token)) != 3 {
+		return &parser.Token{
+			Type:  parser.SimpleError,
+			Value: "wrong argument number for PING command: " + strconv.Itoa(len(t.Value.([]*parser.Token))),
+		}, nil
+	}
+
+	if t.Value.([]*parser.Token)[1].Type != parser.BulkString {
+		return &parser.Token{
+			Type:  parser.SimpleError,
+			Value: "Wrong Key Type: " + t.Value.([]*parser.Token)[1].Type.String(),
+		}, nil
+	}
+
+	if t.Value.([]*parser.Token)[2].Type != parser.BulkString {
+		return &parser.Token{
+			Type:  parser.SimpleError,
+			Value: "Wrong Value Type: " + t.Value.([]*parser.Token)[2].Type.String(),
+		}, nil
+	}
+
+	key := string(t.Value.([]*parser.Token)[1].Value.([]byte))
+	value := t.Value.([]*parser.Token)[2]
+
+	err := h.store.Set(key, value)
+	if err != nil {
+		return nil, err
+	}
+
+	return &parser.Token{Type: parser.SimpleString, Value: "OK"}, nil
+}
+func (h *Handlers) handleGet(t *parser.Token) (*parser.Token, error) {
+	if len(t.Value.([]*parser.Token)) != 2 {
+		return &parser.Token{
+			Type:  parser.SimpleError,
+			Value: "wrong argument number for PING command: " + strconv.Itoa(len(t.Value.([]*parser.Token))),
+		}, nil
+	}
+
+	if t.Value.([]*parser.Token)[1].Type != parser.BulkString {
+		return &parser.Token{
+			Type:  parser.SimpleError,
+			Value: "Wrong Key Type: " + t.Value.([]*parser.Token)[1].Type.String(),
+		}, nil
+	}
+	key := string(t.Value.([]*parser.Token)[1].Value.([]byte))
+
+	value, err := h.store.Get(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return value, nil
 }
